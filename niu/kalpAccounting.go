c83ac1d4684ea4b003a68d4343e092dfc6e6a07a
Abhinav <abhinav1.mishra@mai.io>
2024-10-07 14:09:48 +0530

    fixed spelling mistake

--- niu/kalpAccounting.go
+++ niu/kalpAccounting.go
@@ -1117,7 +1117,7 @@ func (s *SmartContract) TransferFrom(ctx kalpsdk.TransactionContextInterface, da
 	err = kaps.TransferUTXOFrom(ctx, []string{transferFrom.Owner}, []string{transferFrom.Spender}, transferFrom.Receiver, GINI, transferFrom.Amount, "UTXO")
 	if err != nil {
 		return Response{
-			Message:    fmt.Sprintf("unable to tramsfer funds: %v", err),
+			Message:    fmt.Sprintf("unable to transfer funds: %v", err),
 			Success:    false,
 			Status:     "Failure",
 			StatusCode: http.StatusInternalServerError,
