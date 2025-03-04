717db81ef44da8490400cfc0b38ac66db3abcc90
muditp2e <mudit.saxena@mai.io>
2025-03-04 11:36:11 +0530

    fix: updated balanceOf

--- chaincode/smartcontract.go
+++ chaincode/smartcontract.go
@@ -307,25 +307,6 @@ func (s *SmartContract) BalanceOf(ctx kalpsdk.TransactionContextInterface, accou
 	if !isValidAddress {
 		return "0", ginierr.ErrInvalidAddress(account)
 	}
-	amt, err := internal.GetTotalUTXO(ctx, account)
-	if err != nil {
-		return "0", fmt.Errorf("error fetching balance: %v", err)
-	}
-	logger.Log.Infof("total balance:%v account:%s\n", amt, account)
-
-	return amt, nil
-}
-
-func (s *SmartContract) BalanceOf2(ctx kalpsdk.TransactionContextInterface, account string) (string, error) {
-	logger.Log.Infoln("BalanceOf... with arguments", account)
-
-	isValidAddress, err := helper.IsValidAddress(account)
-	if err != nil {
-		return "0", err
-	}
-	if !isValidAddress {
-		return "0", ginierr.ErrInvalidAddress(account)
-	}
 	amt, err := internal.GetTotalUTXOWithPagination(ctx, account)
 	if err != nil {
 		return "0", fmt.Errorf("error fetching balance: %v", err)
