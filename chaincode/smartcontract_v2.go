0dd7d40a68670a0e2cac884868c61952ce66af53
muditp2e <mudit.saxena@mai.io>
2025-05-08 14:20:07 +0530

    fix: added GasFeesTransfer

--- chaincode/smartcontract_v2.go
+++ chaincode/smartcontract_v2.go
@@ -13,7 +13,7 @@ import (
 	"github.com/p2eengineering/kalp-sdk-public/kalpsdk"
 )
 
-func (s *SmartContract) GasFeesTransferSimple(ctx kalpsdk.TransactionContextInterface, gasFeesAccount string, amount string) (bool, error) {
+func (s *SmartContract) GasFeesTransfer(ctx kalpsdk.TransactionContextInterface, gasFeesAccount string, amount string) (bool, error) {
 	signer, e := helper.GetUserId(ctx)
 	if e != nil {
 		err := ginierr.NewInternalError(e, "error getting signer", http.StatusInternalServerError)
@@ -64,89 +64,6 @@ func (s *SmartContract) GasFeesTransferSimple(ctx kalpsdk.TransactionContextInte
 	}
 	if calledContractAddress != s.GetName() {
 		err := ginierr.New("GasFeesTransferSimple should not be called by other contracts", http.StatusUnauthorized)
-		logger.Log.Error(err.FullError())
-		return false, err
-	}
-
-	if gasFeesAccount != constants.KalpFoundationAddress {
-		if err = internal.RemoveUtxoForGasFees(ctx, gasFeesAccount, amount); err != nil {
-			return false, err
-		}
-		if err = internal.AddUtxoForGasFees(ctx, constants.KalpFoundationAddress, amount); err != nil {
-			return false, err
-		}
-		if err := events.EmitTransfer(ctx, gasFeesAccount, constants.KalpFoundationAddress, amount); err != nil {
-			return false, err
-		}
-	}
-	return true, nil
-}
-
-func (s *SmartContract) GasFeesTransferComplex(ctx kalpsdk.TransactionContextInterface, gasFeesAccount string, amount string) (bool, error) {
-	signer, e := helper.GetUserId(ctx)
-	if e != nil {
-		err := ginierr.NewInternalError(e, "error getting signer", http.StatusInternalServerError)
-		logger.Log.Error(err.FullError())
-		return false, err
-	}
-	isGatewayAdmin, err := internal.IsGatewayAdminAddress(ctx, signer)
-	if err != nil {
-		return false, err
-	}
-	if !isGatewayAdmin {
-		err := ginierr.New("signer should be gateway admin for gas fees deduction", http.StatusUnauthorized)
-		logger.Log.Error(err.FullError())
-		return false, err
-	}
-
-	isValidAddress, err := helper.IsUserAddress(gasFeesAccount)
-	if err != nil {
-		return false, err
-	}
-	if !isValidAddress {
-		return false, ginierr.ErrInvalidAddress(gasFeesAccount)
-	}
-
-	amountInt, e := strconv.ParseUint(amount, 10, 64)
-	if e != nil {
-		err := ginierr.NewInternalError(e, "error parsing amount", http.StatusBadRequest)
-		logger.Log.Error(err.FullError())
-		return false, ginierr.ErrInvalidAmount(amount)
-	}
-	if amountInt == 0 {
-		return false, ginierr.ErrInvalidAmount(amount)
-	}
-	var strGatewayMaxGasFee string
-	if strGatewayMaxGasFee, err = s.GetGatewayMaxFee(ctx); err != nil {
-		return false, err
-	}
-	gatewayMaxFeeInt, e := strconv.ParseUint(strGatewayMaxGasFee, 10, 64)
-	if e != nil {
-		err := ginierr.NewInternalError(e, "error parsing GatewayMaxGasFee", http.StatusInternalServerError)
-		logger.Log.Error(err.FullError())
-		return false, ginierr.ErrInvalidAmount(amount)
-	}
-	if amountInt > gatewayMaxFeeInt {
-		return false, ginierr.ErrInvalidAmount(amount)
-	}
-
-	if denied, err := internal.IsDenied(ctx, signer); err != nil {
-		return false, err
-	} else if denied {
-		return false, ginierr.ErrDeniedAddress(signer)
-	}
-	if denied, err := internal.IsDenied(ctx, gasFeesAccount); err != nil {
-		return false, err
-	} else if denied {
-		return false, ginierr.ErrDeniedAddress(gasFeesAccount)
-	}
-
-	calledContractAddress, err := internal.GetCalledContractAddress(ctx)
-	if err != nil {
-		return false, err
-	}
-	if calledContractAddress != s.GetName() {
-		err := ginierr.New("GasFeesTransferComplex should not be called by other contracts", http.StatusUnauthorized)
 		logger.Log.Error(err.FullError())
 		return false, err
 	}
